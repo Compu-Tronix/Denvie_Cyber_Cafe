4d53c86e9aa09749602f20be461770723d8a90f6
manelisi <kabongweinvestments@gmail.com>
2024-03-29 12:06:42 +0000

    index concept

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def app_init():
-    return render_template('index.html')
+    log = 'login'
+    username = 'username'
+    return render_template('index.html', log=log, username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
