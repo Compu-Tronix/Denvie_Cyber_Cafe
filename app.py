ed629e70353c3f475601b307d88cfb2c621707c3
manelisi <kabongweinvestments@gmail.com>
2024-05-10 18:12:01 +0000

    footer and services edit

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 def app_init():
     log = 'register/login'
     username = 'username'
-    return render_template('index.html', log=log, username=username)
+    guest = 'guest'
+    var=0
+    if var ==0:
+        return render_template('index.html', log=log, username=guest)
+    else:
+        return render_template('index.html', log='', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
