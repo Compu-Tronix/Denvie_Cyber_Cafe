a753d7211f1d6b15dc4b94544bf0313780c6aae0
manelisi <kabongweinvestments@gmail.com>
2024-05-29 20:09:51 +0000

    updated base.css

--- static/forms.js
+++ static/forms.js
@@ -1,7 +1,8 @@
 // forms html
+// assignment form
 const assignmentForm = `
 
-<div class="assignment_form">
+<div class="service_form">
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris. Dictum at tempor commodo ullamcorper. Congue eu consequat ac felis. Purus sit amet luctus venenatis lectus magna. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim. Quisque egestas diam in arcu cursus euismod quis viverra. Felis eget nunc lobortis mattis aliquam faucibus purus in massa. At volutpat diam ut venenatis tellus in. Pretium nibh ipsum consequat nisl vel pretium lectus quam.
 </p>
@@ -22,8 +23,6 @@ const assignmentForm = `
 </div>
 
 `
-
-
 
 // replace more_info_container functions
 //assignment
