f58f37e1c5e9992a3b576091fc91cbbd7fe624be
manelisi <kabongweinvestments@gmail.com>
2024-06-18 18:16:53 +0000

    updated user_log_forms

--- static/forms.js
+++ static/forms.js
@@ -30,4 +30,64 @@ function assignment_form() {
     document.getElementById("more_info_container").innerHTML = assignmentForm
   }
 
+//registration form
+const registration_form = `
 
+                
+                <form action="/register" method="POST" class="popup-form">
+                    
+                    <button id="close_form" class="close_form_btn">
+                      X
+                    </button>   
+                    <input type="text" placeholder="name" name="username" required class="input">
+                    <input type="email" placeholder="email" name="email" required class="input">
+                    <input type="password" placeholder="password" name="password" required class="input">
+                    <input type="password" placeholder="confirm password" name="confirm_password" class="input">
+                    <div class="form_buttons_container">
+    
+                        <input type="submit" value="register" class="form_btn">
+
+                        <button class="form_btn" onclick="login(document.getElementById('user_log_form'))">
+                          login
+                        </button>
+
+                    </div>
+                    
+                </form>
+
+`
+const login_form = `
+
+                
+                <form action="/login" method="POST" class="popup-form">
+                    <button id="close_form" class="close_form_btn">
+                      X
+                    </button>
+                    <input type="text" placeholder="name" name="username" required class="input">
+                    <input type="password" placeholder="password" name="password" required class="input">
+
+                    <div class="form_buttons_container">
+    
+                        <input type="submit" value="login" class="form_btn">
+                        
+                        <button class="form_btn" onclick="register(document.getElementById('user_log_form'))">
+                          register
+                        </button>
+   
+                    </div>
+                    
+                    
+                </form>
+
+`
+
+//login, and registration forms function
+//registration form function
+function register() {
+  document.getElementById("user_log_form").innerHTML = registration_form
+}
+
+//login form function
+function login() {
+  document.getElementById("user_log_form").innerHTML = login_form
+}
